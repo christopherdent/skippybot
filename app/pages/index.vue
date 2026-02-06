bead14d0fd7971492d2e135c3689d6ade7eda35f
christopher dent <christopherdent01@gmail.com>
2026-02-06 12:07:58 -0500

    skippybot skeleton

---
+++ app/pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="text-center mt-6">
+    <p class="text-xl text-gray-800">Tailwind and Nuxt UI are working 🧙‍♂️</p>
+    <UButton class="mt-4">Push Me</UButton>
+  </div>
+</template>
