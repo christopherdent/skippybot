d0c02b56b9d6b10daf5f431f5de9ceccee969f47
= <christopher.dent@opscompass.com>
2026-02-06 17:53:24 -0500

    disable lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,19 +3,13 @@ import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt({
   rules: {
-    // Turn off strict formatting rules
+    // Turn off formatting rules completely
     'indent': 'off',
     'vue/html-indent': 'off',
-    'prettier/prettier': [
-      'warn', // or 'off' to fully disable
-      {
-        // override formatting rules if needed
-        printWidth: 120,
-        tabWidth: 2,
-        useTabs: false,
-        semi: false,
-        singleQuote: true
-      }
-    ]
+    'vue/singleline-html-element-content-newline': 'off',
+    'vue/multiline-html-element-content-newline': 'off',
+
+    // Optional: kill prettier entirely
+    'prettier/prettier': 'off',
   }
 })
